74181a8cd8d1e72ec85fe611f66d93f1859b6895
m-ghiringhelli <mghiring@gmail.com>
2022-06-09 11:48:09 -0700

    successfully fetches data

--- sql/setup.sql
+++ sql/setup.sql
@@ -19,8 +19,8 @@ INSERT INTO roster (name, number, position, age, country) VALUES
 ('Dario Zuparic', 13, 'Defender', 30, 'Croatia'),
 ('Justin Rasmussen', 14, 'Defender', 23, 'United States'),
 ('Christhian Paredes', 22, 'Midfielder', 24, 'Paraguay'),
-('David Ayala', 7, 'Midfielder', 19, 'Argentina'),
+('David Ayala', 24, 'Midfielder', 19, 'Argentina'),
 ('Yimmi Chara', 23, 'Midfielder', 31, 'Colombia'),
 ('Marvin Loria', 44, 'Midfielder', 25, 'Costa Rica'),
-('Dairon Asprilla', 27, 'Forward', 20, 'Colombia'),
+('Dairon Asprilla', 27, 'Forward', 30, 'Colombia'),
 ('Nathan Fogaca', 99, 'Forward', 23, 'Brazil');
